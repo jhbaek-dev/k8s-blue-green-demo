c263b22e9c6bbc854bd37635bbf9cfc1e327501e
백준호 (Junho Baek) <jh.baek1@hanwha.com>
2025-08-24 21:07:10 +0900

    feat: add log

--- service-a/main.go
+++ service-a/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"net/http"
 	"os"
@@ -11,37 +12,60 @@ import (
 const VERSION = "v1.0.2"
 
 func main() {
+	log.Printf("[DEBUG] Service-A 시작 중... 버전: %s", VERSION)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[DEBUG] 요청 수신: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+
 		w.Header().Set("Content-Type", "text/html")
 		rootVersion := os.Getenv("ROOT_VERSION")
 		serviceBHost := os.Getenv("SERVICE_B_HOST")
 		serviceCHost := os.Getenv("SERVICE_C_HOST")
-		
+
+		log.Printf("[DEBUG] 환경 변수 확인 - ROOT_VERSION: %s, SERVICE_B_HOST: %s, SERVICE_C_HOST: %s", rootVersion, serviceBHost, serviceCHost)
+
 		serviceBVersion := "unknown"
 		if serviceBHost != "" {
+			log.Printf("[DEBUG] Service-B에 연결 시도: %s:3000", serviceBHost)
 			resp, err := http.Get("http://" + serviceBHost + ":3000/version")
 			if err == nil {
 				body, _ := io.ReadAll(resp.Body)
 				serviceBVersion = string(body)
 				resp.Body.Close()
+				log.Printf("[DEBUG] Service-B 버전 응답 성공: %s", serviceBVersion)
+			} else {
+				log.Printf("[DEBUG] Service-B 연결 실패: %v", err)
 			}
+		} else {
+			log.Printf("[DEBUG] SERVICE_B_HOST가 설정되지 않음")
 		}
-		
+
 		serviceCVersion := "unknown"
 		if serviceCHost != "" {
+			log.Printf("[DEBUG] Service-C에 TCP 연결 시도: %s:3000", serviceCHost)
 			conn, err := net.Dial("tcp", serviceCHost+":3000")
 			if err == nil {
+				log.Printf("[DEBUG] Service-C TCP 연결 성공, 버전 요청 전송")
 				conn.Write([]byte("version"))
 				buf := make([]byte, 1024)
 				n, _ := conn.Read(buf)
 				serviceCVersion = string(buf[:n])
 				conn.Close()
+				log.Printf("[DEBUG] Service-C 버전 응답 성공: %s", serviceCVersion)
+			} else {
+				log.Printf("[DEBUG] Service-C TCP 연결 실패: %v", err)
 			}
+		} else {
+			log.Printf("[DEBUG] SERVICE_C_HOST가 설정되지 않음")
 		}
-		
-		fmt.Fprintf(w, "Service-A Version: %s<br>Service-B Version: %s<br>Service-C Version: %s<br>Root Version: %s", 
+
+		response := fmt.Sprintf("Service-A Version: %s<br>Service-B Version: %s<br>Service-C Version: %s<br>Root Version: %s",
 			VERSION, serviceBVersion, serviceCVersion, rootVersion)
+
+		log.Printf("[DEBUG] 응답 반환: %s", response)
+		fmt.Fprint(w, response)
 	})
-	
-	http.ListenAndServe(":3000", nil)
+
+	log.Printf("[DEBUG] HTTP 서버 시작: 포트 3000에서 대기 중...")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
